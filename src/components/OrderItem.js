c7214adede2fa600f728354115ed7fc6137ad248
“Drmingler” <kayymedia@gmail.com>
2020-05-11 14:34:09 +0200

    feat : Add view and delete functionality to orderitem component

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,15 +1,34 @@
 import React from "react";
-
+import { withRouter } from "react-router-dom";
+import {connect} from "react-redux";
+import { handleDeleteOrder } from "../actions/orders";
 class OrderItem extends React.Component {
+  handleViewOrderDetails = e => {
+    e.preventDefault();
+    const { orderId } = this.props;
+    this.props.history.push(`/orderdetails/${orderId}`);
+  };
+  handleOrderDelete = e => {
+    const { dispatch, orderId } = this.props;
+    e.preventDefault();
+    dispatch(handleDeleteOrder(orderId));
+  };
+
   render() {
-      const {order, orderId} = this.props;
-    return <div>
+    const { order, orderId } = this.props;
+    return (
+      <div>
         <p>{order.customerName}</p>
         <p>{orderId}</p>
-        <button> View Order </button>
-        <button> delete Order </button>
-    </div>;
+        <button onClick={this.handleViewOrderDetails}> View Order </button>
+        <button onClick={this.handleOrderDelete}> delete Order </button>
+      </div>
+    );
   }
 }
 
-export default OrderItem
+function mapStateToProps({ orders }) {
+  return { orders };
+}
+
+export default withRouter(connect(mapStateToProps)(OrderItem));
