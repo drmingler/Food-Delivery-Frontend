21d004115aaa1768c47cd95a85955be3b0fa9b8d
“Drmingler” <kayymedia@gmail.com>
2020-05-11 19:16:22 +0200

    feat : Add Helper function to serialize food data to a readable format

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -6,3 +6,26 @@ export const getOrderInfo = (orders, routeId) => {
   const result = { ...specificOrder };
   return result[0];
 };
+
+
+export function foodDropDown(foods) {
+ return  foods.map(food => ({
+    key: food.id,
+    text: `${food.food_name} $${food.price}`,
+    value: food.id,
+  }));
+}
+
+
+
+export function calculateTotal(array, foodItems) {
+  let total = 0;
+  array.forEach(number => {
+    foodItems.forEach(item => {
+      if (item.id === number) {
+        total += item.price;
+      }
+    });
+  });
+  return total;
+}
